6340968e7c3e40ebaf8fa23a8917c04beb2b8cc3
adityakawale07 <adityakawale42@gmail.com>
2026-09-15 23:20:24 +0530

    Add files via upload

---
+++ Java Study/Functions and methods/sumnum.java
@@ -0,0 +1,13 @@
+public class sumnum {
+
+    public static void SUM(int num1,int num2)
+    {
+        int summ=num1+num2;
+        System.out.println("The sum of two numbers is:"+summ);
+    }
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        SUM(num1,num2);
+    }
+}
