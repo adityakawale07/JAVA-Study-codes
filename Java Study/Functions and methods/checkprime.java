c790447e964ca75be87f8c20a9b1b92bfae8a6ea
adityakawale07 <adityakawale42@gmail.com>
2026-09-21 00:13:58 +0530

    Add files via upload

---
+++ Java Study/Functions and methods/checkprime.java
@@ -0,0 +1,17 @@
+public class checkprime {
+    public static boolean isPrime(int n)
+    {
+        for(int i=2;i<n-1;i++)
+        {
+            if(i%n==0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(7));
+    }
+}
